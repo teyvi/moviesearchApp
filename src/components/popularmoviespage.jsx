bf8116a33472fc8845393d05c5ddc377b1b859ab
KojoFocus <ernestobimpeh9@gmail.com>
2024-03-15 12:08:44 +0000

    added movie details to the top rated series and popular movies

--- src/components/popularmoviespage.jsx
+++ src/components/popularmoviespage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const PopularMovies = () => {
   const url =
@@ -28,19 +29,20 @@ const PopularMovies = () => {
         <h1>Popular Movies</h1>
           <div className="flex overflow-auto gap-x-2 ">
             {popularmovies.map((item, index) => (
-              <div key={index} className="flex-shrink-0">
-                <div className="w-48 h-auto p-4 rounded-md">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                    alt="movie"
-                    className="h-64 rounded-md"
-                  />
-                  <h2 className="mt-2 text-base font-semi-bold truncate h-7 ">
-                    {item.original_title}
-
-                  </h2>
-                </div>
+              <div key={index} className="flex-shrink-0 ">
+              <Link to={'/moviedetailspage/' + item.id}>
+              <div className=" w-48 h-auto p-4 rounded-md ">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                  alt="movie"
+                  className="h-64 rounded-md "
+                />
+                <h2 className="mt-2 text-base font-semibold truncate h-7 ">
+                  {item.original_title}
+                </h2>
               </div>
+              </Link>
+            </div>
             ))}
           </div>
         </div>
