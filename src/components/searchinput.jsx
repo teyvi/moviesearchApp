b6856613b377be8645c926f0bb0848c57264df45
teyvi <44750503+teyvi@users.noreply.github.com>
2024-04-06 16:57:40 +0000

    style

---
+++ src/components/searchinput.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Searchinput = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const gotoSearchPage = (event) => {
+    console.log(event);
+    if (event.key === "Enter") {
+      navigate(`/search?query=${searchQuery}`);
+    }
+  };
+
+  return (
+    <div className="">
+      <input
+        type="text"
+        value={searchQuery}
+        className="w-full h-11 mt-1 rounded-[8rem] text-stone-950 outline-none px-20 placeholder:text-slate-500"
+        placeholder="Search for movies and series....."
+        onChange={handleSearch}
+        onKeyDown={gotoSearchPage}
+      ></input>
+      <div className="bg-white">
+        <button className="font-semibold text-stone-950">Search</button>
+      </div>
+    </div>
+  );
+};
+export default Searchinput;
